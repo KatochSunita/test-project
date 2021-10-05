2abf2d088240095ac0296922643897679bcfdc12
KatochSunita <72437802+KatochSunita@users.noreply.github.com>
2021-10-05 16:06:09 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    // colors:{
+    //   indigo: '#ff6347',
+    //  white: colors.white,
+    //   red: colors.red,
+    //   green: colors.green,
+    // },
+    // width: {
+    //   '1/3': '30%',
+    //   '100': '100%',
+     
+      
+     
+    // }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
